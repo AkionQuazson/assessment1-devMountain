08f0b0eeb634d3fd664f8971c11ec370acf6ec83
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2022-08-24 17:53:46 -0600

    altered variable names in pw

--- password.js
+++ password.js
@@ -1,4 +1,4 @@
-let password = 'asdf1213';
+let password = 'asdFjk1213';
 /*
     As requested, the minimum length is 10, and a letter and a number are both required.
     In addition, there is a maximum length of 20, and the space character is not allowed in the password.
@@ -27,17 +27,20 @@ else {
         else if (!capLetter && passChars[i].toLowerCase() !== passChars[i].toUpperCase() && passChars[i] === passChars[i].toUpperCase()) {
             capLetter = true;
         }
-        else if (!spc && passChars[i] === " ") {
-            spc = true;
+        else if (!spaces && passChars[i] === " ") {
+            spaces = true;
         }
     }
     if (!num) {
         console.log("There is no number in the password");
     }
-    else if (!ltr) {
-        console.log("There is no letter in the password");
+    else if (!letter) {
+        console.log("There is no lowercase letter in the password");
     }
-    else if (spcs) {
+    else if (!capLetter) {
+        console.log("There is no uppercase letter in the password");
+    }
+    else if (spaces) {
         console.log("There is a space in the password");
     }
     else {
